2a5b0085b251ddcf5df4917299a70a89e6e10862
ZILLY777 <sashaa08032000@gmail.com>
2023-06-26 21:23:16 +0300

    Logger improve

--- src/main/java/com/example/MtsTestExam/service/serviceImpl/BankAccountServiceImpl.java
+++ src/main/java/com/example/MtsTestExam/service/serviceImpl/BankAccountServiceImpl.java
@@ -27,7 +27,7 @@ public class BankAccountServiceImpl implements BankAccountService {
     private final BankAccountRepository bankAccountRepository;
     private final ClientRepository clientRepository;
     private final BankAccountMapper bankAccountMapper;
-   @Value("#{'${currency}'.split(',')}")
+    @Value("#{'${currency}'.split(',')}")
     private List<String> currencyList;
     private final Logger logger = LoggerFactory.getLogger(ClientController.class);
 
@@ -35,32 +35,38 @@ public class BankAccountServiceImpl implements BankAccountService {
     @Override
     public UUID postBankAccount(BankAccountDto bankAccountDto) {
         if(!currencyList.contains(bankAccountDto.getAccountCurrency())){
+            logger.error("Недопустимая валюта {}", bankAccountDto.getAccountCurrency());
             throw new InvalidCurrencyException();
         }
         if(clientRepository.findClientById(bankAccountDto.getClientId()).isEmpty()){
+            logger.error("Клиент не найден clientId = {}", bankAccountDto.getClientId());
             throw new ClientNotFoundException();
         }
+        logger.info("Сохранение аккаунта успешно");
         return bankAccountRepository.save(bankAccountMapper.bankAccountDtoToBankAccount(bankAccountDto)).getAccountNumber();
     }
 
     @Override
     public List<BankAccountDto> getClientListOfBankAccounts(UUID clientId) {
         if(clientRepository.findClientById(clientId).isEmpty()){
-            logger.info("Client not found");//уточнить
+            logger.error("Клиент не найден clientId = {}", clientId);
             throw new ClientNotFoundException();
         }
+        logger.info("Список выдан");
         return bankAccountMapper.bankAccountListToBankAccountListDto(bankAccountRepository.findBankAccountsByClientId(clientId));
     }
 
     @Override
     public void deleteBankAccount(UUID clientId, UUID bankAccountNumber) {
         if(clientRepository.findClientById(clientId).isEmpty()){
-
+            logger.error("Клиент не найден clientId = {}", clientId);
             throw new ClientNotFoundException();
         }
         if(bankAccountRepository.findBankAccountByAccountNumber(bankAccountNumber).isEmpty()){
+            logger.error("Банк не найден bankAccountNumber = {}", bankAccountNumber);
             throw new BankAccountNotFoundException();
         }
+        logger.info("Аккаунт удален");
         bankAccountRepository.deleteBankAccountByClientIdAndBankAccountId(clientId, bankAccountNumber);
 
     }
--- src/main/java/com/example/MtsTestExam/service/serviceImpl/ClientServiceImpl.java
+++ src/main/java/com/example/MtsTestExam/service/serviceImpl/ClientServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.MtsTestExam.service.serviceImpl;
 
+import com.example.MtsTestExam.controller.ClientController;
 import com.example.MtsTestExam.entity.dto.ClientDto;
 import com.example.MtsTestExam.entity.tables.Client;
 import com.example.MtsTestExam.exception.*;
@@ -7,6 +8,8 @@ import com.example.MtsTestExam.mapper.ClientMapper;
 import com.example.MtsTestExam.repository.repositoryInterface.ClientRepository;
 import com.example.MtsTestExam.service.serviceInterfaces.ClientService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -22,27 +25,34 @@ public class ClientServiceImpl implements ClientService {
     private final ClientMapper clientMapper;
     @Value("#{'${documentType}'.split(',')}")
     private List<String> documentTypeList;
+    private final Logger logger = LoggerFactory.getLogger(ClientController.class);
 
     @Override
     public UUID postClient(ClientDto clientDto) {
         if(clientDto.getName().isEmpty() || clientDto.getSurname().isEmpty() ){
+            logger.warn("Фамилия или Имя введены неправильно {} {}", clientDto.getSurname(), clientDto.getName());
             throw new EmptyFIOException();
         }
         if(!documentTypeList.contains(clientDto.getDocument())){
+            logger.warn("Тип документа не установлен {}", clientDto.getDocument());
             throw new InvalidDocTypeException();
         }
         if(clientDto.getDocumentData().isEmpty()){
+            logger.warn("Данные документа не верны {}", clientDto.getDocumentData());
             throw new InvalidDocDataException();
         }
         Optional<Client> client= clientRepository.findClientByDocumentTypeAndData(clientDto.getDocument(), clientDto.getDocumentData());
         if(client.isPresent()){
+            logger.warn("Клиент уже зарегистрирован {}", client);
             throw new ClientAlreadyExistException();
         }
+        logger.info("Клиент зарегистрирован {}", clientDto);
         return clientRepository.save(clientMapper.clientDtoToClient(clientDto)).getId();
     }
 
     @Override
     public List<ClientDto> getAllClients() {
+        logger.info("Выдан список клиентов");
         return clientMapper.clientListToClientListDto(clientRepository.findAllClient());
     }
 
@@ -52,6 +62,7 @@ public class ClientServiceImpl implements ClientService {
         if(client.isPresent()){
             clientRepository.deleteClient(clientId);
         }else{
+            logger.error("Клиент не найден clientId = {}", clientId);
             throw new ClientNotFoundException();
         } //можно подумать над проверкой точно ли удалено
 
